edaba196b48fbc79274cd243181cf308f3ee13b2
Beatrice Marietta <143585093+beatricemarietta@users.noreply.github.com>
2023-09-09 14:12:08 +0700

    Update Decision Tree.py

--- Decision Tree.py
+++ Decision Tree.py
@@ -84,6 +84,9 @@ for i in df.columns:
 targetnames = df['Selector'].unique()
 tgname = np.array(['Pasien','Non Pasien'])
 
+(df['Selector'] == 1).sum()
+(df['Selector'] == 2).sum()
+
 #Data Determination
 d_X = df.drop(columns=['Selector'])
 d_Y = df['Selector']
@@ -95,18 +98,61 @@ Xtrain, Xtest, Ytrain, Ytest = train_test_split(d_X, d_Y, test_size=0.2, random_
 clf = DecisionTreeClassifier()
 clf.fit(Xtrain, Ytrain)
 
-#Predictinon and Accuracy Checking 
-Ypred = clf.predict(Xtest)
-accuracy = accuracy_score(Ytest, Ypred)
-print("Accuracy:", accuracy)
+#Predictinon and Accuracy Checking Data Training
+Ypred_tr = clf.predict(Xtrain)
+accuracy_tr = accuracy_score(Ytrain, Ypred_tr)
+print("Accuracy:", accuracy_tr)
 
-Ypredtr = clf.predict(Xtrain)
-accuracytr = accuracy_score(Ytrain, Ypredtr)
-print("Accuracy:", accuracytr)
+#Predictinon and Accuracy Checking Data Testing
+Ypred_ts = clf.predict(Xtest)
+accuracy_ts = accuracy_score(Ytest, Ypred_ts)
+print("Accuracy:", accuracy_ts)
 
 #Clasification Report
 print("\nClassification Report\n")
 print(classification_report(Ytest, Ypred, target_names=tgname))
+
+#Confusion Matrix Data Training
+Ypred_tr = clf.predict(Xtrain)
+print('Actual Pasien & Predicted Pasien:', ((Ytrain == 1) & (Ypred_tr == 1)).sum())
+print('Actual Pasien & Predicted Non Pasien:', ((Ytrain == 1) & (Ypred_tr == 2)).sum())
+print('Actual Non Pasien & Predicted Pasien:', ((Ytrain == 2) & (Ypred_tr == 1)).sum())
+print('Actual Non Pasien & Predicted Non Pasien:', ((Ytrain == 2) & (Ypred_tr == 2)).sum())
+conf_matrix_tr = confusion_matrix(Ytrain, Ypred_tr, labels=[1,2])
+confusion_tr = pd.DataFrame(conf_matrix_tr, columns=["Predicted Pasien", "Predicted Non Pasien"], index=["Actual Pasien", "Actual Non Pasien"])
+print('\n------------- Confusion Matrix Data Training ------------\n')
+print(confusion_tr)
+print('---------------------------------------------------------\n')
+accuracy_tr = accuracy_score(Ytrain, Ypred_tr)
+correct_tr = ((Ytrain == 1) & (Ypred_tr == 1)).sum()+((Ytrain == 2) & (Ypred_tr == 2)).sum()
+wrong_tr = ((Ytrain == 1) & (Ypred_tr == 2)).sum()+((Ytrain == 2) & (Ypred_tr == 1)).sum()
+print('\n--- Data Training Accuracy ---\n')
+print('Correct Classified  :', correct_tr)
+print('Accuracy            :', round(accuracy_tr*100,2),'%')
+print('Correct Classified  :', wrong_tr)
+print('Error               :', round(100-accuracy_tr*100,2),'%')
+print('------------------------------')
+
+#Confusion Matrix Data Testing
+Ypred_ts = clf.predict(Xtest)
+print('Actual Pasien & Predicted Pasien:', ((Ytest == 1) & (Ypred_ts == 1)).sum())
+print('Actual Pasien & Predicted Non Pasien:', ((Ytest == 1) & (Ypred_ts == 2)).sum())
+print('Actual Non Pasien & Predicted Pasien:', ((Ytest == 2) & (Ypred_ts == 1)).sum())
+print('Actual Non Pasien & Predicted Non Pasien:', ((Ytest == 2) & (Ypred_ts == 2)).sum())
+conf_matrix_ts = confusion_matrix(Ytest, Ypred_ts, labels=[1,2])
+confusion_ts = pd.DataFrame(conf_matrix_ts, columns=["Predicted Pasien", "Predicted Non Pasien"], index=["Actual Pasien", "Actual Non Pasien"])
+print('\n------------- Confusion Matrix Data Testing -------------\n')
+print(confusion_ts)
+print('---------------------------------------------------------\n')
+accuracy_ts = accuracy_score(Ytest, Ypred_ts)
+correct_ts = ((Ytest == 1) & (Ypred_ts == 1)).sum()+((Ytest == 2) & (Ypred_ts == 2)).sum()
+wrong_ts = ((Ytest == 1) & (Ypred_ts == 2)).sum()+((Ytest == 2) & (Ypred_ts == 1)).sum()
+print('\n--- Data Testing Accuracy ---\n')
+print('Correct Classified  :', correct_ts)
+print('Accuracy            :', round(accuracy_ts*100,2),'%')
+print('Correct Classified  :', wrong_ts)
+print('Error               :', round(100-accuracy_ts*100,2),'%')
+print('-----------------------------')
 
 #Decision Tree Visualization
 import matplotlib.pyplot as plt
